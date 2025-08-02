6bfdee87d65d335dacec4585ac8307a1710ef163
Dhanalakshmi-Nagaraju <sonuswarna813@gmail.com>
2025-08-03 01:26:49 +0530

    vercel frontend url updated

--- server.js
+++ server.js
@@ -1,23 +1,52 @@
+// import 'dotenv/config';
+// import express from 'express'
+// import cors from 'cors'
+// import userRouter from './routes/userRoutes.js';
+// import connectDB from './configs/mongodb.js';
+// import imageRouter from './routes/imageRoutes.js';
+
+// // App Config
+// const PORT = process.env.PORT || 4000
+// const app = express();
+// await connectDB()
+
+// // Intialize Middlewares
+// app.use(express.json())
+// app.use(cors())
+
+// // API routes
+// app.use('/api/user',userRouter)
+// app.use('/api/image',imageRouter)
+
+// app.get('/', (req,res) => res.send("API Working"))
+
+// app.listen(PORT, () => console.log('Server running on port ' + PORT));
+
+
+
 import 'dotenv/config';
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import userRouter from './routes/userRoutes.js';
 import connectDB from './configs/mongodb.js';
 import imageRouter from './routes/imageRoutes.js';
 
 // App Config
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 const app = express();
-await connectDB()
+await connectDB();
 
 // Intialize Middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors({
+  origin: 'https://ai-image-frontend-mu.vercel.app/', // ✅ Replace with your deployed frontend URL
+  credentials: true
+}));
 
 // API routes
-app.use('/api/user',userRouter)
-app.use('/api/image',imageRouter)
+app.use('/api/user', userRouter);
+app.use('/api/image', imageRouter);
 
-app.get('/', (req,res) => res.send("API Working"))
+app.get('/', (req, res) => res.send("API Working"));
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
